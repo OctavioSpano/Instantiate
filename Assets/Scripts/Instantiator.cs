5bfef12d671abad45ac497c953beda2f26dc6498
OctavioSpano <tutispano@gmail.com>
2022-05-09 12:04:49 -0300

    9/5 cree instantiate

---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+
+    public int cant;
+    public void CloneObject()
+    
+    {
+        int i = 0;
+        while (i < cant)
+        {
+            Instantiate(objectToClone);
+            i++;
+        }
+    }
+}
